ee4e41d286903153ad0fa6c26cc124431ef9e7a0
NeilRaiden <NeilRaidenLLC@gmail.com>
2024-11-12 09:28:18 -0500

    add script for building wallpapers

---
+++ wallpapers/script.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# Note: 
+# before running this script start "konsole" in full-screen (F11).
+
+# commands to display text-wallpapers:
+# a. English Phonemic layout:
+vim -u vrcPhonem EnglishPhonemic-full.txt
+# b. Writer layouts:
+vim -u vrcASERT WriterPlusASERTH-full.txt
+vim -u vrcWriterPlus WriterPlusQWERTY-altgr.txt
+vim -u vrcWriter WriterQWERTY-altgr.txt
+# c. Shavian layouts:
+vim -u vrcShaw ShavianImperial-full.txt
+vim -u vrcShaw ShavianQWERTY-full.txt
+
